5180ae926b60ac1d317ae13ac689bf4e0d4882ee
Matthew Ellison <matthewcellison@gmail.com>
2020-04-18 00:45:04 -0500

    first

---
+++ local_project/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { HelloWorld } from "my_npm_pkg2";
+const Test = () => {
+  return (
+    <div>
+      <HelloWorld name="test" />
+    </div>
+  );
+};
---
+++ my_npm_pkg/src/Components/hello.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { HelloProps } from "../types";
+
+export const HelloWorld = ({ name }: HelloProps) => <div>{name}</div>;
