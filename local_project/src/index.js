69c897a5a86410f8b2b6ffaed42445328614b21a
Matthew Ellison <matthewcellison@gmail.com>
2020-04-20 00:30:06 -0500

    setup

--- local_project/src/index.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { HelloWorld } from "my_npm_pkg2";
-const Test = () => {
-    return (React.createElement("div", null,
-        React.createElement(HelloWorld, null)));
-};
--- local_project/src/index.tsx
+++ local_project/src/index.tsx
@@ -1,9 +1,19 @@
-import * as React from "react";
 import { HelloWorld } from "my_npm_pkg2";
-const Test = () => {
-  return (
-    <div>
-      <HelloWorld name="test" />
-    </div>
-  );
+import ReactDOM from "react-dom";
+import React from "react";
+var loadme = function() {
+  const Test = () => {
+    return (
+      <div>
+        <HelloWorld name="test" />
+      </div>
+    );
+  };
+
+  const domContainer = document.querySelector("#app");
+  ReactDOM.render(<Test />, domContainer);
 };
+
+window.addEventListener("load", function() {
+  loadme();
+});
